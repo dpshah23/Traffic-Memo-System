1f427dd3dced17f15076d292360f61a10febcafd
Kavan Bhavsar <129761522+KavanBhavsar35@users.noreply.github.com>
2024-02-24 22:54:26 +0530

    App for admin

---
+++ Admin Panel/adminpanel/adminpanelapp1/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from adminpanelapp1 import views
+
+urlpatterns = [
+   path('home/',view=views.admin,name='admin'),
+   path('',views.login,name="login"),
+   path('send_verification',views.verify,name="verification"),
+   path('sendmail',views.sendmail,name="sendmail"),
+   path('verify',views.verify,name="verify"),
+   path('login',views.login,name="login")
+   
+]
